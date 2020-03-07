679a350e97d8fa047ab588b7f96f728d07b757b2
Andrey Moshkin <iandreu.m@outlook.com>
2020-03-07 14:07:40 +0300

    first commit

---
+++ setup-git-v1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+git init
+git config --global user.name "Andrey Moshkin"
+git config --global user.email "iandreu.m@outlook.com"
+sudo git config --system core.editor nano
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=25000'
+git config --global push.default simple
+
+
+echo "################################################################"
+echo "###################    T H E   E N D      ######################"
+echo "################################################################"
